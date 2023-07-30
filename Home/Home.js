1cac3e8f994ad9f27b4bf84615e2622461918c3a
Zaid Sobani <zaidsobani@gmail.com>
2023-07-30 11:17:10 +0300

    Commiting every file

---
+++ Home/Home.js
@@ -0,0 +1,59 @@
+
+document.getElementById("home4_click1").onclick = function () { 
+    document.getElementById("para1").style.display = "block"
+    document.getElementById("para2").style.display = "none"
+    document.getElementById("para3").style.display = "none"
+    document.getElementById("para4").style.display = "none"
+    document.getElementById("home4_click1").style.backgroundColor= "#ed1c24"
+    document.getElementById("home4_click1").style.color= "#fff"
+    document.getElementById("home4_click2").style.backgroundColor= "#fff"
+    document.getElementById("home4_click2").style.color= "#000"
+    document.getElementById("home4_click3").style.backgroundColor= "#fff"
+    document.getElementById("home4_click3").style.color= "#000"
+    document.getElementById("home4_click4").style.backgroundColor= "#fff"
+    document.getElementById("home4_click4").style.color= "#000"
+}
+document.getElementById("home4_click2").onclick = function () {
+    document.getElementById("para2").style.display = "block"
+    document.getElementById("para1").style.display = "none"
+    document.getElementById("para3").style.display = "none"
+    document.getElementById("para4").style.display = "none"
+    document.getElementById("home4_click2").style.backgroundColor= "#ed1c24"
+    document.getElementById("home4_click2").style.color= "#fff"
+    document.getElementById("home4_click1").style.backgroundColor= "#fff"
+    document.getElementById("home4_click1").style.color= "#000"
+    document.getElementById("home4_click3").style.backgroundColor= "#fff"
+    document.getElementById("home4_click3").style.color= "#000"
+    document.getElementById("home4_click4").style.backgroundColor= "#fff"
+    document.getElementById("home4_click4").style.color= "#000"
+}
+document.getElementById("home4_click3").onclick = function () {
+    document.getElementById("para3").style.display = "block"
+    document.getElementById("para1").style.display = "none"
+    document.getElementById("para2").style.display = "none"
+    document.getElementById("para4").style.display = "none"
+    document.getElementById("home4_click3").style.backgroundColor= "#ed1c24"
+    document.getElementById("home4_click3").style.color= "#fff"
+    document.getElementById("home4_click1").style.backgroundColor= "#fff"
+    document.getElementById("home4_click1").style.color= "#000"
+    document.getElementById("home4_click2").style.backgroundColor= "#fff"
+    document.getElementById("home4_click2").style.color= "#000"
+    document.getElementById("home4_click4").style.backgroundColor= "#fff"
+    document.getElementById("home4_click4").style.color= "#000"
+}
+document.getElementById("home4_click4").onclick = function () {
+    document.getElementById("para4").style.display = "block"
+    document.getElementById("para1").style.display = "none"
+    document.getElementById("para2").style.display = "none"
+    document.getElementById("para3").style.display = "none"
+    document.getElementById("home4_click4").style.backgroundColor= "#ed1c24"
+    document.getElementById("home4_click4").style.color= "#fff"
+    document.getElementById("home4_click1").style.backgroundColor= "#fff"
+    document.getElementById("home4_click1").style.color= "#000"
+    document.getElementById("home4_click2").style.backgroundColor= "#fff"
+    document.getElementById("home4_click2").style.color= "#000"
+    document.getElementById("home4_click3").style.backgroundColor= "#fff"
+    document.getElementById("home4_click3").style.color= "#000"
+
+
+}
