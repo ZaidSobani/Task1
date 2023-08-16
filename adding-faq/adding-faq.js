cc2d3dd40385b8c969a2efe978cf10a4092e6e99
Zaid Sobani <zaidsobani@gmail.com>
2023-08-16 17:45:24 +0300

    updated

--- adding-faq/adding-faq.js
+++ adding-faq/adding-faq.js
@@ -1,23 +1,35 @@
 
 var faqs;
+var faqToEditIndex;
+class Faq {
+    constructor(question, answer, ids) {
+        this.question = question;
+        this.answer = answer;
+        this.ids = ids;
+    }
+}
 
 window.onload = function() {
-    faqs = JSON.parse(localStorage.getItem("faqs"));
+    faqStorage = localStorage.getItem("faqs");
 
-    if(!faqs) {
+    if(faqs) {
+        faqs = JSON.parse(faqStorage);
+    }
+    else {
         faqs = new Array ();
+        faqs.push(new Faq("question1", "answer1", Math.random()));
+        faqs.push(new Faq("question2", "answer2", Math.random()));
+        faqs.push(new Faq("question3", "answer3", Math.random()));
+
         localStorage.setItem("faqs", JSON.stringify(faqs));
     }
 
 
-    const faqOutput = document.getElementById("faqs");
-    /*for(let i=0; i<faqs.length; i++) {
-        faqOutput.innerHTML = faqOutput.innerHTML + "<div><span>-" + (i+1) + "-</span><span>" + 
-        faqs[i].question + "</span><span>" + faqs[i].answer + "</span></div>"; 
-    }*/
-
-    /*faqOutput.innerHTML = faqOutput.innerHTML + "<table style='border-bottom:none;'><tr><th><span>" + "Index" + "</span></th><th><span>" + 
-    "Question" + "</span></th><th><span>" + "Answer" + "</span></th></tr></table>";*/
+    const faqsOutput = document.getElementById("faqTable1");
+    for(let i = 0; i < faqs.length; i++) {
+       faqsOutput.innerHTML = faqsOutput.innerHTML + buildFaqHtml(faqs[i], i);
+       deleteFaq(faqs[i],i)
+    }
 }
 
 function addFaq() {
@@ -25,74 +37,102 @@ function addFaq() {
     document.getElementById("addToTable").style.display = "block";
     document.getElementById("editTable").style.display = "none";
 
+    document.getElementById("questions_field").value = "";
+    document.getElementById("answers_field").value = "";
 }
-function edit() {
+function edit(index) {
+    faqToEditIndex = index;
+    faq = faqs[index];
+
     document.getElementById("ques_container").style.display = "block";
     document.getElementById("addToTable").style.display = "none";
     document.getElementById("editTable").style.display = "block";
-    console.log(test13.id);
-    document.getElementById("questions_field").value = faqs[faqs.length - 1].question;
-    document.getElementById("answers_field").value = faqs[faqs.length - 1].answer;
+
+    document.getElementById("questions_field").value = faq.question;
+    document.getElementById("answers_field").value = faq.answer;
 }
+
 function createFaq() {
-    let input1 = document.getElementById("questions_field").value;
-    let input2 = document.getElementById("answers_field").value;
+    const input1 = document.getElementById("questions_field").value;
+    const input2 = document.getElementById("answers_field").value;
+    const randomId = Math.random();
+    const faq = new Faq(input1, input2, randomId);
 
-    faqs.push({question:input1, answer:input2});
+    faqs.push(faq);
 
-    const faqOutput = document.getElementById("faqTable1");
-    /*for(let i=0; i<faqs.length; i++) {
-        faqOutput.innerHTML = faqOutput.innerHTML + "<div><span>-" + (i+1) + "-</span><span>" + 
-        faqs[i].question + "</span><span>" + faqs[i].answer + "</span></div>"; 
-    }*/
-    faqOutput.innerHTML = faqOutput.innerHTML + "<tr id='test12' class='faqTable2'><td><span>" +faqs.length + 
-    "</span></td><td><span id='test13.id'>" + faqs[faqs.length - 1].question + 
-    "</span></td><td><span id='test13_id'>" + faqs[faqs.length - 1].answer + 
-    "</span></td><td><span><button onclick='edit("+ indexOfId() +")'>" + 
-    "Edit" + "</button></span></td></tr>";
-
-    //faqOutput.appendChild(document.createElement("tr"));
+    const faqsOutput = document.getElementById("faqTable1");
+    faqsOutput.innerHTML = faqsOutput.innerHTML + buildFaqHtml(faq, (faqs.length - 1))
+    // faqOutput.innerHTML = faqOutput.innerHTML + "<tr id='test12' class='faqTable2'><td><span>" +faqs.length + 
+    // "</span></td><td><span [id]='x'>" + faqs[faqs.length - 1].question + 
+    // "</span></td><td><span id=''>" + faqs[faqs.length - 1].answer + 
+    // "</span></td><td><span><button onclick='edit()'>" + 
+    // "Edit" + "</button></span></td></tr>";
 
     localStorage.setItem("faqs", JSON.stringify(faqs));
 
-    
-    function indexOfId() {
-        // id = "ques" +faqs.indexOf(faqs[faqs.length]);
-        var test131 = document.getElementsByTagName("tr");
-        for (let j= 0; j < test131.length; j++) {
-            var test13 = test131[j];
-            test13.setAttribute("id", j + 1);
-            test13.id = j+1;
-            return test13.id;
-        }
-    }    
-    console.log(indexOfId());
+    // function indexOfId() {
+    //     // id = "ques" +faqs.indexOf(faqs[faqs.length]);
+    //     var test131 = document.getElementsByTagName("tr");
+    //     for (var j= 0; j < test131.length; j++) {
+    //         var test13 = test131[j];
+    //         test13.setAttribute("id", j + 1);
+    //         test13.id = j+1;
+    //         return "id is" + test13.id;
+    //     }
+    // }    
+    // var x= setTimeout(indexOfId, 2000);
+    // console.log(setTimeout(indexOfId, 2000));
 
-    
-    document.getElementById("questions_field").value = "";
-    document.getElementById("answers_field").value = "";
     document.getElementById("ques_container").style.display = "none";
 
     //document.getElementById("bottomBorder").style.borderBottom = "none";    
 }
 function editFaq() {
-    let input1 = document.getElementById("questions_field").value;
-    let input2 = document.getElementById("answers_field").value;
+    const faq = faqs[faqToEditIndex];
+    faq.question = document.getElementById("questions_field").value;
+    faq.answer = document.getElementById("answers_field").value;              //no need to push replacing existing values
 
-    faqs.push({question:input1, answer:input2});
+    const faqOutput = document.getElementById("faq-" + faq.ids);
+    faqOutput.innerHTML = buildFaqInnerHtml(faq, faqToEditIndex);
+    
+    localStorage.setItem("faqs", JSON.stringify(faqs));
 
-    document.getElementById("test12_a").innerHTML = document.getElementById("questions_field").value;
-    document.getElementById("test12_b").innerHTML = document.getElementById("answers_field").value
-
-    document.getElementById("questions_field").value = "";
-    document.getElementById("answers_field").value = "";
     document.getElementById("ques_container").style.display = "none";
+}
+
+function removeFaq(removeIndex) {
+    const faq =faqs[removeIndex];
+    
+    const rowNumber = document.getElementById("faq-" + faq.ids);
+    rowNumber.remove()
+    
+    const idsS = faq.ids;
+
+    
+    var faqIndex = faqs.findIndex(test => test.ids === idsS);
+    faqs.splice(faqIndex, 1);
+    console.log(faqIndex)
+    
+    localStorage.setItem("faqs", JSON.stringify(faqs));
+    //document.getElementById("faqTable1").deleteRow(removeIndex + 1);
 }
 
 function closeFaq() {
     document.getElementById("ques_container").style.display = "none";
     document.getElementById("addToTable").style.display = "none";
+    document.getElementById("editTable").style.display = "none";
 }
 
+function buildFaqHtml(faq, index) {
+    return "<tr id='faq-" + faq.ids + "' class='faqTable1' >" + buildFaqInnerHtml(faq, index) + "</tr>";
+}
+function buildFaqInnerHtml(faq, index) {
+    return "<td><span>" + (index + 1) + "</span></td><td><span>" + faq.question + "</span></td><td><span>" + faq.answer + 
+    "</span></td><td><span><button class='editFaq' onclick='edit("+ index +")'>" + "Edit" + 
+    "</button></span></td><td><span><button class='removeFaq' onclick='removeFaq("+ index +")'><span class='material-symbols-outlined'>" + 
+    "delete" + "</span></button></span></td>"; 
+}
 
-
+function deleteFaq(faq ,index) {
+    return index
+}
